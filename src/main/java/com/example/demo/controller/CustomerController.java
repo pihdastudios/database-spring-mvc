7bdf980a7d4895478339868eb6054169011d30c5
pihda <62319714+pihdastudios@users.noreply.github.com>
2022-01-23 22:27:17 +0700

    update

--- src/main/java/com/example/demo/controller/CustomerController.java
+++ src/main/java/com/example/demo/controller/CustomerController.java
@@ -1,14 +1,26 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.example.demo.dao.CustomerDAO;
+
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
-	@RequestMapping("/list")
-	public String listCustomers(Model model) {
-		return "list-customers";
-	}
+    @Autowired
+    public void setCustomerDAO(CustomerDAO customerDAO) {
+        this.customerDAO = customerDAO;
+    }
+
+    private CustomerDAO customerDAO;
+
+    @RequestMapping("/list")
+    public String listCustomers(Model model) {
+        var customers = customerDAO.findAllByOrderByLastNameAsc();
+        model.addAttribute("customers", customers);
+        return "list-customers";
+    }
 }
--- src/main/java/com/example/demo/entity/Customer.java
+++ src/main/java/com/example/demo/entity/Customer.java
@@ -1,11 +1,6 @@
 package com.example.demo.entity;
 
-import jakarta.persistence.Table;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 @Table(name = "customer")
